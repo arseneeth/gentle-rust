4c95a3a2acdc9c386f1ffbf01aee792f8e32e239
0x332 <kinspotting@protonmail.com>
2019-10-31 12:49:29 +0800

    add: better move

--- structs-enums-matching/move2/src/main.rs
+++ structs-enums-matching/move2/src/main.rs
@@ -1,10 +1,25 @@
-fn dump(s: String){
+// Not working example
+
+// fn dump(s: String){
+// 	println!("{}", s);
+// }
+
+// fn main() {
+//     let s1 = "hello dolly".to_string();
+
+//     dump(s1);
+//     println!("s1 {}", s1);
+// }
+
+// working example
+
+fn dump(s: &str){
 	println!("{}", s);
 }
 
 fn main() {
     let s1 = "hello dolly".to_string();
 
-    dump(s1);
+    dump(&s1);
     println!("s1 {}", s1);
 }
--- structs-enums-matching/move2/target/debug/deps/move2-342aa99291a99701.d
+++
@@ -1,5 +0,0 @@
-/home/x332/Projects/gentle-rust/structs-enums-matching/move2/target/debug/deps/move2-342aa99291a99701: src/main.rs
-
-/home/x332/Projects/gentle-rust/structs-enums-matching/move2/target/debug/deps/move2-342aa99291a99701.d: src/main.rs
-
-src/main.rs:
