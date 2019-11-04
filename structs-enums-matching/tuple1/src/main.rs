455669b72b81f0cbe566d55e139be1daded6035c
0x332 <kinspotting@protonmail.com>
2019-11-04 16:58:42 +0800

    add: tuple

---
+++ structs-enums-matching/tuple1/src/main.rs
@@ -0,0 +1,17 @@
+fn add_mul(x: f64, y: f64) -> (f64,f64) {
+	(x + y, x * y)
+}
+
+fn main() {
+	let t = add_mul(2.0, 10.0);
+
+	// can debug print
+	println!("t {:?}", t);
+
+	// can 'index' the values
+	println!("add {} mul {}", t.0, t.1); 
+
+	// can extract values
+	let (add, mul) = t;
+	println!("add {} mul {}", add, mul);
+}
